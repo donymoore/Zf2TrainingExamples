6a83cbe82b634bf527c4bea54eef25e1741cc721
donymoore <dony.moore@gmail.com>
2014-02-12 10:22:57 -0600

    calc stuff from josh

--- module/Calculator/src/Calculator/Controller/StringCalculatorController.php
+++ module/Calculator/src/Calculator/Controller/StringCalculatorController.php
@@ -15,8 +15,42 @@ class StringCalculatorController extends AbstractActionController
 
     public function CalculateAction()
     {
-        return new ViewModel();
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $firstNumber = is_null($request->getPost()->firstNumber) ? 0 : (float) $request->getPost()->firstNumber;
+            $secondNumber = is_null($request->getPost()->secondNumber) ? 0 : (float) $request->getPost()->secondNumber;
+            $operator = $request->getPost()->operator;
+            $answer = "ERR";
+
+            switch($operator) {
+                case "add":
+                    $answer = $firstNumber + $secondNumber;
+                    break;
+                case "subtract":
+                    $answer = $firstNumber - $secondNumber;
+                    break;
+                case "multiply":
+                    $answer = $firstNumber * $secondNumber;
+                    break;
+                case "divide":
+                    if ($secondNumber == 0) {
+                        $answer = "Divide by zero oh shi--";
+                    } else {
+                        $answer = $firstNumber / $secondNumber;
+                    }
+                    break;
+                case "modulo":
+                    $answer = $firstNumber % $secondNumber;
+                    break;
+                default:
+                    $answer = "Please select an operator.";
+            }
+
+            return array('answer'=>$answer);
+
+        } else {
+            return new ViewModel();
+        }
     }
 
 }
-
